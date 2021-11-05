d4b61308ba1ee6a13483beb92ba52e30903246f1
Steven Armend <stevenarmend@gmail.com>
2021-11-04 23:31:50 -0600

    11/4/2021 set up database and set up Books route + book Search

--- index.js
+++ index.js
@@ -28,3 +28,26 @@ app.get("/books",(req, res)=>{
         res.status(500).send();
     })
 })
+app.get("/books/:BookID", (req, res) => {
+    let bookID = req.params.BookID;
+    //   console.log(pk);
+    let myQuery = `Select * 
+    FROM Book 
+    LEFT JOIN Authors
+    on Authors.AuthorID = Book.AuthorID
+    WHERE BookID = ${bookID}`;
+  
+    db.executeQuery(myQuery)
+      .then((result) => {
+        // console.log("result", result);
+        if (result[0]) {
+          res.send(result[0]);
+        } else {
+          res.status(404).send(`bad request`);
+        }
+      })
+      .catch((err) => {
+        console.log("Error in /movies/:pk", err);
+        res.status(500).send();
+      });
+    })
