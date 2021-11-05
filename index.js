38b5c9d0d124002d618d511ea0c74eb6b33b23f2
Steven Armend <stevenarmend@gmail.com>
2021-11-04 22:29:27 -0600

    Initial Comit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require(`express`);
+
+const app = express();
+
+app.listen(5000, ()=>{console.log("App is running on port 5000")})
+
+app.get("/hi",(req, res)=>{
+    res.send("Hello World")})
+
+app.get("/",(req, res)=>{
+    res.send("API Is Running")})
+
+// app.post()
+// app.put()
